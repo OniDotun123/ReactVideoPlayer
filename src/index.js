0cf29cc1c2a3e8811555b28ded3d42cef884362c
Adedotun <onidotun99@gmail.com>
2017-07-18 14:53:24 -0400

    Render text on to container successfully

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // Create A new Component that should produce some HTML
 const App = function(){
   return <div>Hi</div>
@@ -6,3 +9,4 @@ const App = function(){
 
 
 // Take this component's generated HTML and put it on the DOM.
+ReactDOM.render(<App />, document.querySelector('.container'));
