ead0aa8e40eba5d1173776cd87135315a5a9861c
Adedotun <onidotun99@gmail.com>
2017-07-18 14:29:41 -0400

    First lines of react. Testing the waters

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// Create A new Component that should produce some HTML
+const App = function(){
+  return <div>Hi</div>
 
-import App from './components/app';
-import reducers from './reducers';
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML and put it on the DOM.
