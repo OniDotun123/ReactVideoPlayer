f0ed0c3433cfe23dd00b6efe4001da5635b99c3a
Adedotun <onidotun99@gmail.com>
2017-07-18 15:28:03 -0400

    create page component files and install youtube search API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyAWbYXUmqT0yg3qjpKPapM1kcVrSAaoXwg'
+
 // Create A new Component that should produce some HTML
-const App = function(){
+const App = () =>{
   return <div>Hi</div>
 
 }
